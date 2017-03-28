c078fa4e23fefddb9af403179318dd37be1c700d
ges0909 <gerrit.schrader@gmail.com>
2017-03-28 07:13:34 +0200

    bean validation refined

--- src/de/infinit/emp/service/Service.java
+++ src/de/infinit/emp/service/Service.java
@@ -6,20 +6,18 @@ import java.util.logging.Logger;
 import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
 import javax.validation.Validator;
-import javax.validation.ValidatorFactory;
 
 public class Service {
 	static final Logger LOG = Logger.getLogger(SensorService.class.getName());
-	static final ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
-	static final Validator validator = factory.getValidator();
+	static final Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
 
 	public <T> boolean isValid(T bean) {
 		Set<ConstraintViolation<T>> violations = validator.validate(bean);
-		if (violations.isEmpty()) {
+		if (violations.isEmpty())
 			return true;
-		}
 		for (ConstraintViolation<T> v : violations) {
-			LOG.warning("constraint violation: " + v.getPropertyPath().toString() + ": " + v.getMessage());
+			LOG.warning("constraint violation: " + v.getPropertyPath().toString() + "=" + v.getInvalidValue() + ": "
+					+ v.getMessage());
 		}
 		return false;
 	}
